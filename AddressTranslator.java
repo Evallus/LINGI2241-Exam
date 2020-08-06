801a5b5d6920cde12fadbecc839d0fb3d1a4b342
Romain Loncelle <loncelle.romain@gmail.com>
2020-08-06 14:36:56 +0200

    - Constructor update (WIP); - Readme update (assume addresses).

--- AddressTranslator.java
+++ AddressTranslator.java
@@ -1,20 +1,55 @@
-class AddressTranslator {   
+import java.util.*;
+
+class AddressTranslator {
+
+	//Class variables
+	int pageSize;
+	int numTLBRows;
+	List<int> pageTable;
+    LinkedHashMap<Integer,Integer> tlb;
+
 	// Constructor   
 	// pageSize: size of a page in bytes. Can be either 4096 or 8192.   
-	// numTLBRows: number of rows in the TLB. Can be either 4 or 8 or 16.   
-	public AddressTranslator(int pageSize, int numTLBRows) { … } 
+	// numTLBRows: number of rows in the TLB (translation lookaside buffer). Can be either 4 or 8 or 16.
+	public AddressTranslator(int pageSize, int numTLBRows) {
+	    //Page size intialisation
+        if(pageSize == 4096 || pageSize == 8192){
+            this.pageSize = pageSize;
+
+            //Page table creation
+            pageTable = new LinkedList<int>(this.pageSize);
+        }else{
+            System.out.println("Wrong page size ! The page size should be 4096 or 8192.");
+        }
+
+        //numTLBRows initialisation
+        if(numTLBRows == 4 || numTLBRows == 8 || numTLBRows == 16){
+            this.numTLBRows = numTLBRows;
+
+            //TLB creation
+            tlb = new LinkedHashMap<Integer,Integer>(this.numTLBRows);
+        }else{
+            System.out.println("Wrong number of rows in the TLB (translation lookaside buffer) ! The number of rows in the TLB should be 4, 8 or 16.");
+        }
+	}
  
 	// Sets an entry in the page table VPN->PPN. There is only one flag.   
 	// Should throw an IllegalArgumentException exception if the page numbers 
 	// are not in a valid range.   
-	public void setPageTableEntry(int VPN, int PPN, boolean isPresent) { … } 
+	public void setPageTableEntry(int VPN, int PPN, boolean isPresent) {
+
+    }
  
 	// Translates the virtual address and returns the corresponding physical address.   
 	// Must throw an IllegalArgumentException exception if there is no page 
 	// present at that address.   
-	public int translate(int virtualAddress) { … } 
+	public int translate(int virtualAddress) {
+
+    }
  
 	// Returns the number of TLB hits   
-	public int getNumberOfHits() { … }  
+	public int getNumberOfHits() {
+
+    }
 } 
  
--- README.md
+++ README.md
@@ -3,4 +3,7 @@ The text file named “readme.txt” where you describe your cache management st
 
 This project will use Java 14.
 
+We will assume that all addresses are positive int values, i.e. numbers between 0 and 231 − 1.
+We will also assume that the physical memory has maximum size, i.e. all physical addresses between 0 and 231 − 1 are valid.
+
 Cache management strategy :
