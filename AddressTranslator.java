8d19e0741d59b4db09d67c64feaa5f84e35f02c0
Romain Loncelle <loncelle.romain@gmail.com>
2020-08-07 12:17:28 +0200

    - Inner class for entry; - Size of the page table calculated (offset etc); - Page table now a list of Entry;

--- AddressTranslator.java
+++ AddressTranslator.java
@@ -3,30 +3,30 @@ import java.util.*;
 class AddressTranslator {
 
 	//Class variables
-	int pageSize;
-	int numTLBRows;
-	List<int> pageTable;
-    LinkedHashMap<Integer,Integer> tlb;
+	private int pageSize;
+	private int numTLBRows;
+	private List<Entry> pageTable;
+    private LinkedHashMap<Integer,Integer> tlb;
 
 	// Constructor   
 	// pageSize: size of a page in bytes. Can be either 4096 or 8192.   
 	// numTLBRows: number of rows in the TLB (translation lookaside buffer). Can be either 4 or 8 or 16.
 	public AddressTranslator(int pageSize, int numTLBRows) {
-	    //Page size intialisation
+	    // Page size intialisation
         if(pageSize == 4096 || pageSize == 8192){
             this.pageSize = pageSize;
 
-            //Page table creation
-            pageTable = new LinkedList<int>(this.pageSize);
+            // Page table creation
+            pageTable = new LinkedList<Entry>(pageTableSize(this.pageSize));
         }else{
             System.out.println("Wrong page size ! The page size should be 4096 or 8192.");
         }
 
-        //numTLBRows initialisation
+        // numTLBRows initialisation
         if(numTLBRows == 4 || numTLBRows == 8 || numTLBRows == 16){
             this.numTLBRows = numTLBRows;
 
-            //TLB creation
+            // TLB creation
             tlb = new LinkedHashMap<Integer,Integer>(this.numTLBRows);
         }else{
             System.out.println("Wrong number of rows in the TLB (translation lookaside buffer) ! The number of rows in the TLB should be 4, 8 or 16.");
@@ -50,6 +50,58 @@ class AddressTranslator {
 	// Returns the number of TLB hits   
 	public int getNumberOfHits() {
 
+    }
+
+    private int pageTableSize(int pageSize){
+	    if(pageSize == 4096){
+	        return 1048575;
+        }
+	    return 524287;
+    }
+
+    // Inner class
+    private class Entry {
+
+	    // Class variables
+	    private int vpn;
+	    private int ppn;
+	    private boolean present;
+
+        // Constructor
+        // vpn: virtual page number used for the translation of this entry.
+        // ppn: physical page number used for the translation of this entry.
+        // present: tells if the entry is accessible (true) or not (false).
+        public Entry(int vpn, int ppn, boolean present) {
+            this.vpn = vpn;
+            this.ppn = ppn;
+            this.present = present;
+        }
+
+        // Getters
+        public int getVpn() {
+            return vpn;
+        }
+
+        public int getPpn() {
+            return ppn;
+        }
+
+        public boolean isPresent() {
+            return present;
+        }
+
+        // Setters
+        public void setVpn(int vpn) {
+            this.vpn = vpn;
+        }
+
+        public void setPpn(int ppn) {
+            this.ppn = ppn;
+        }
+
+        public void setPresent(boolean present) {
+            this.present = present;
+        }
     }
 } 
  
